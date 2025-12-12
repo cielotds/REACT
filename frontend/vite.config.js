5c60463a15431270124a5870bbfdd6743216334b
cielotds <tadascielo01@gmail.com>
2025-12-12 17:17:54 +0800

    Add Vercel configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
+  // This ensures correct paths in production
+  base: './',
 })
